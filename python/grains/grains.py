37e7c02f024b0e48cca7ce0a10f3cc5b6609db1d
RenAlv08 <renan.c.alves03@gmail.com>
2023-04-29 22:21:12 -0300

    exercise "Grains" resolution

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,17 @@
 def square(number):
-    pass
+    """:param number: int - the number of the square
+        :return: int - how many grains were on a given square"""
+    if (1 > number or number > 64):
+        raise ValueError("square must be between 1 and 64")
+    else:
+        return 2 ** (number - 1)
 
 
 def total():
-    pass
+    """:return: int - number of grains on the chessboard"""
+    square_number = 1
+    number_of_grains = 2 ** (square_number - 1)
+    while square_number != 64:
+        square_number += 1
+        number_of_grains += 2 ** (square_number - 1)
+    return number_of_grains
